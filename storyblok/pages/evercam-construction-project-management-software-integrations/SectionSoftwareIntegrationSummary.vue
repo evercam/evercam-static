0c099f4a20130e20ea847423766ff0fc9626441f
Yonathan Evan C <yonathanevan@gmail.com>
2023-03-27 13:55:07 +0700

    Fix Image

--- storyblok/pages/evercam-construction-project-management-software-integrations/SectionSoftwareIntegrationSummary.vue
+++ storyblok/pages/evercam-construction-project-management-software-integrations/SectionSoftwareIntegrationSummary.vue
@@ -18,20 +18,20 @@ defineProps({ blok: Object })
             <div class="container tick-image text-center">
                 <div class="row">
                     <div class="col-md-4 ">
-                        <img src=" https://evercam.io/wp-content/uploads/2021/02/connect.png" alt="">
+                        <img src="https://evercam.io/wp-content/uploads/2021/02/connect.png" alt="Image">
                         <h3>Connect<br>
     Everyone</h3>
     <p>Seamless data<br>
     transfer reduces friction throughout your project</p>
                     </div>
                     <div class="col-md-4 ">
-                        <img src=" https://evercam.io/wp-content/uploads/2021/02/give-context.png" alt="">
+                        <img src="https://evercam.io/wp-content/uploads/2021/02/give-context.png" alt="Image">
                         <h3>Streamlined<br>
     Communication</h3>
     <p>Add an image to your snag list, observations or RFIs</p>
                     </div>
                     <div class="col-md-4 ">
-                        <img src=" https://evercam.io/wp-content/uploads/2021/02/image-1.png" alt="">
+                        <img src="https://evercam.io/wp-content/uploads/2021/02/image-1.png" alt="Image">
                         <h3>The Power<br>
     of Image</h3>
     <p>Use images to verify the data within your PM software &amp; support the status of the project</p>
