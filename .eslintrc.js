b683424641595c3721bdce54796ab14176a13988
Halim <halim.bouguedra@gmail.com>
2023-06-01 18:22:23 +0100

    Add linting pre-commit hook

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+  extends: ["@nuxtjs/eslint-config-typescript", "plugin:prettier/recommended"],
+  plugins: [],
+  rules: {
+    "vue/no-multiple-template-root": "off",
+    "vue/require-default-prop": "off",
+    "prettier/prettier": [
+      "error",
+      {
+        trailingComma: "es5",
+        tabWidth: 2,
+        semi: false,
+        singleQuote: false,
+      },
+    ],
+  },
+}
