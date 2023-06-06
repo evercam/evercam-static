b60d25a29200c52d39f47a92f3a279fa45711d90
Yonathan Evan C <yonathanevan@gmail.com>
2023-06-07 00:39:42 +0700

    Revert "Setup Vuex store for localDomains state"
    This reverts commit 39badf3107a06d1df2daed9123788eaf2b172f6a.

---
+++ app 2.vue
@@ -0,0 +1,16 @@
+<template>
+  <NuxtLayout>
+    <SeoKit />
+    <!-- a. Generates browser screenshots for every page -->
+    <!-- <OgImageScreenshot /> -->
+    <!-- b. Generate saotir images for every page (uses the default template) -->
+    <!-- <OgImageStatic /> -->
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script setup>
+//defineRobotMeta()
+// const  {locale}  = useI18n();
+// console.log(locale);
+</script> 
---
+++ app_backup.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtLayout>
+    <SeoKit />
+    <!-- a. Generates browser screenshots for every page -->
+    <!-- <OgImageScreenshot /> -->
+    <!-- b. Generate saotir images for every page (uses the default template) -->
+    <!-- <OgImageStatic /> -->
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<!-- <script lang="ts" setup>
+defineRobotMeta()
+</script> -->
