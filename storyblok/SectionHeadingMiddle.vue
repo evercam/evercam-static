7d6a1b80ffd21649c7e2ba606b6fc5d6c37991a7
Yonathan Evan C <yonathanevan@gmail.com>
2023-07-06 02:34:09 +0700

    Location details

--- storyblok/SectionHeadingMiddle.vue
+++ storyblok/SectionHeadingMiddle.vue
@@ -1,8 +1,12 @@
 <script setup>
 const props = defineProps({ blok: Object });
 const resolvedContent = computed(() => renderRichText(props.blok.content));
+const resolvedDetails = computed(() =>
+  renderRichText(props.blok.details)
+);
 
-let containerClass = props.blok.container_class_option
+let sectionId = props.blok.id_option ? props.blok.id_option : "marketing-up";
+let containerClass = props.blok.container_class_option;
 
 const assetSource = props.blok.video
   ? props.blok.video
@@ -24,12 +28,16 @@ if (isYoutube) {
 </script>
 
 <template>
-  <section :class="containerClass" v-editable="blok" id="marketing-up">
+  <section :class="containerClass" v-editable="blok" :id="sectionId">
     <div class="container">
       <h2 v-if="blok.name">{{ blok.name }}</h2>
       <div class="text-center" v-html="resolvedContent"></div>
+      <div class="section-left-and-right" v-html="resolvedDetails"></div>
       <div v-if="blok.video" class="box-shadow">
-        <div v-for="blok in blok.video" class="embed-responsive embed-responsive-16by9">
+        <div
+          v-for="blok in blok.video"
+          class="embed-responsive embed-responsive-16by9"
+        >
           <iframe
             class="embed-responsive-item lazyloaded"
             v-if="blok.url"
