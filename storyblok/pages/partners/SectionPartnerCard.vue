23972a871cbc6123f1f67f0db453ef743d2e476b
Yonathan Evan C <yonathanevan@gmail.com>
2023-07-07 10:52:50 +0700

    Update SectionPartnerCard.vue

--- storyblok/pages/partners/SectionPartnerCard.vue
+++ storyblok/pages/partners/SectionPartnerCard.vue
@@ -46,43 +46,4 @@ const checkMap = props.blok.google_maps_embed ? "col-lg-6 partner" : "col-lg-6 p
       referrerpolicy="no-referrer-when-downgrade"
     ></iframe>
   </div>
-  <!-- <div v-editable="blok" v-else class="col-lg-6 partner no-map">
-    <div class="text-center">
-      <a :href="blok.partner_website" target="_blank">
-        <img
-          v-if="blok.partner_logo"
-          :src="blok.partner_logo"
-          :alt="blok.name"
-        />
-      </a>
-    </div>
-    <h2>{{ blok.name }}</h2>
-    <p>
-      {{ blok.description }}
-    </p>
-    <div v-html="description"></div>
-    <p>
-      <strong>Areas Covered:<br /> </strong>{{ blok.area }}
-    </p>
-    <p>
-      <strong>Contact:</strong><br />
-      {{blok.address}}
-      <br />
-      <a v-if="blok.phone" :href="`tel://+`+ blok.phone">+{{blok.phone}}</a><br />
-      <a v-if="blok.email" :href="`mailto:`+ blok.email"
-        >{{blok.email}}</a
-      ><br />
-      <a v-if="blok.partner_website" :href="blok.partner_website">{{blok.name}}</a>
-    </p>
-    <iframe
-      v-if="blok.google_maps_embed"
-      :src="blok.google_maps_embed"
-      width="600"
-      height="450"
-      style="border: 0"
-      allowfullscreen=""
-      loading="lazy"
-      referrerpolicy="no-referrer-when-downgrade"
-    ></iframe>
-  </div> -->
 </template>
