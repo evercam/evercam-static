86eceb3345eb95c969b3178f18ac938e1e8f9b63
Halim <halim.bouguedra@gmail.com>
2023-07-07 14:17:38 +0100

    Test precommit hook

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,10 +1,10 @@
 <script setup>
 const { slug } = useRoute().params;
-const { locale } = useI18n();
+const { locale } = useI18n()
 
 const story = await useAsyncStoryblok(
   slug && slug.length > 0 ? slug.join('/') : 'home',
-  { 
+  {
     version: 'draft',
     language: locale.value,
    }
